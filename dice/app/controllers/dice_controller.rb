ada6a6166a1bbb07cc15b3877e428a4a81002f1d
Julie Soliman <julie.soliman@gmail.com>
2014-01-27 19:13:38 -0600

    Homework 3 complete

--- dice/app/controllers/dice_controller.rb
+++ dice/app/controllers/dice_controller.rb
@@ -7,11 +7,11 @@ class DiceController < ApplicationController
     @sum = @roll.first + @roll.last
 
     if @sum == 7 || @sum == 11 then
-      @feedback = "You win!"
+      @feedback = "YOU WIN!"
       @button_label = "Start Over"
       @button_action= {:action => "index"}
     elsif @sum == 2 || @sum == 3 || @sum == 12 then
-      @feedback = "You Lose"
+      @feedback = "YOU LOSE"
       @button_label = "Start Over"
       @button_action= {:action => "index"}
     else
@@ -25,8 +25,23 @@ class DiceController < ApplicationController
     faces = [1,2,3,4,5,6]
     @pair_of_dice = faces.product(faces)
     @roll = @pair_of_dice.sample;
-    # @button_action= {:action => "roll", :goal => @sum}
-    @button_action= {:action => "roll", :method => :get}
+    @sum = @roll.first + @roll.last
+
+    @goal = Integer(params[:goal])
+
+    if @sum == @goal then
+      @feedback = "YOU WIN!"
+      @button_label = "Start Over"
+      @button_action= {:action => "index"}
+    elsif @sum == 7
+      @feedback = "YOU LOSE"
+      @button_label = "Start Over"
+      @button_action= {:action => "index"}
+    else
+      @feedback = "Not yet! Keep trying"
+      @button_label = "Roll the dice"
+      @button_action= {:action => "roll", :goal => @goal}
+    end
   end
 
 end
--- dice/config/routes.rb
+++ dice/config/routes.rb
@@ -2,6 +2,11 @@ Dice::Application.routes.draw do
   root 'dice#index'
 
   post("/start_roll" => "dice#start_roll", :as => "dice")
-  # get({:action=>"roll", :controller=>"dice"})
+  post({"/" => "dice#index"})
+  get "/:sum" => "dice#roll"
+
+  # TODO: POST WITH PARAMS
+  post({"/roll" => "dice#roll"})
+  # get "/goal=:sum" => 'dice#roll', :as => 'roll'
 end
 
